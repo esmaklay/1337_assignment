1fb9d521d50ba97138cc2859b8e6bed7928617d2
Esma Klay <esma_klay@hotmail.com>
2022-06-27 14:31:57 +0200

    added filter and small description in readme

--- 1337_employees/src/components/EmployeesList.js
+++ 1337_employees/src/components/EmployeesList.js
@@ -3,7 +3,7 @@ import GetEmployees from '../data/GetEmployees';
 import { Card } from 'react-bootstrap';
 import './employeesList.css';
 
-function EmployeesList(props) {
+const EmployeesList = (props) => {
 	const [ employees, setEmployees ] = useState([]);
 	const { data, loading } = GetEmployees();
 
@@ -19,23 +19,25 @@ function EmployeesList(props) {
 		return employees.sort((a, b) => (a[props.sortValue] > b[props.sortValue] ? 1 : -1));
 	};
 
+	const sortedAndFilteredEmployees = () => {
+		return sortedEmployees().filter((x) => x.name.toLowerCase().includes(props.filterValue));
+	};
+
 	if (loading) {
 		return <p>Loading employees...</p>;
 	}
 
-	return sortedEmployees().map((employee) => {
+	return sortedAndFilteredEmployees().map((employee) => {
 		return (
-			<>
-				<Card border="secondary" key={employee.email}>
-					<Card.Img variant="top" src={employee.imagePortraitUrl} />
-					<Card.Body>
-						<Card.Title>{employee.name}</Card.Title>
-						<Card.Text>{employee.office}</Card.Text>
-					</Card.Body>
-				</Card>
-			</>
+			<Card border="secondary" key={employee.email}>
+				<Card.Img variant="top" src={employee.imagePortraitUrl} />
+				<Card.Body>
+					<Card.Title>{employee.name}</Card.Title>
+					<Card.Text>{employee.office}</Card.Text>
+				</Card.Body>
+			</Card>
 		);
 	});
-}
+};
 
 export default EmployeesList;
--- 1337_employees/src/components/Main.js
+++ 1337_employees/src/components/Main.js
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import EmployeeList from './EmployeesList';
-import { Container } from 'react-bootstrap';
+import { Container, Navbar, Offcanvas } from 'react-bootstrap';
 
 const Main = () => {
 	const [ sortValue, setSortValue ] = useState('name');
-	const [ filterValue, setfilterValue ] = useState('');
+	const [ filterValue, setFilterValue ] = useState('');
 
 	const handleSelect = (e) => {
 		setSortValue(e.target.value);
 	};
 
+	const handleInput = (e) => {
+		setFilterValue(e.target.value);
+	};
+
 	return (
 		<div>
+			<input id="filter" name="filter" type="text" value={filterValue} onChange={handleInput} />
+			Sort by:
 			<select onChange={handleSelect}>
 				<option value="name">Name</option>
 				<option value="office">Office</option>
 			</select>
 			<Container>
-				<EmployeeList sortValue={sortValue} />
+				<EmployeeList sortValue={sortValue} filterValue={filterValue} />
 			</Container>
 		</div>
 	);
